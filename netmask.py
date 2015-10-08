7025beb261895353786021f83f3b79f58ddce461
nashgul <sinp13@gmail.com>
2015-10-08 09:37:17 +0200

    Primer commit. Calculadora sin regexp

---
+++ netmask.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def principal():
+    print("Introduce la IP (acepto notación CIDR: 'IP/[0-32]' )")
+    ip_principal_decimal = pedir_ip()
+    contenido = ip_principal_decimal.rpartition('/')
+    notacion_es_CIDR = True if contenido[1] == '/' else False
+# Si hay número CIDR:
+    if notacion_es_CIDR:
+        ip_es_valida = True if comprobar_ip(contenido[0]) else False
+# Si el número CIDR es válido y la IP también
+        if ip_es_valida and 0 <= int(contenido[2]) <= 32:
+# guardo IP y CIDR
+            ip_principal = contenido[0]
+            CIDR = contenido[2]
+            print("IP: %s" % ip_principal)
+            print("CIDR: %s" % CIDR)
+        elif not ip_es_valida:
+# si la IP no es válida, la pido de nuevo junto con la máscara.
+            error_ip(contenido[0])
+            principal()
+# si no es válida la notación CIDR, pido máscara.
+        else: 
+            print("[-] Número CIDR %s incorrecto." % contenido[2])
+# si no se ha metido notación CIDR
+    elif not notacion_CIDR:
+        pass
+
+def error_ip(IP):
+    print("[-] IP '%s' errónea!! Pon más atención la próxima vez." % IP)
+
+def comprobar_ip(IP):
+    valores_IP = IP.split('.')
+    valido = True if (len(valores_IP) == 4) else False
+# Si hay numero IP
+    if valido:
+        for x in valores_IP:
+            if type(x) == int:
+# Si hay un número entero IP fuera de rango
+                if 0 > int(x) > 255:
+                    error_ip(IP)
+                    principal()
+# Si los 4 número son correctos
+    return True
+def pedir_ip():
+    IP = raw_input("IP: ")
+    return IP
+
+principal()    
